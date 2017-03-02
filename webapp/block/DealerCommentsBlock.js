28d22d47526db1f8efc011f4ebea4fb56b376d4b
alisdair73 <alisdair73@gmail.com>
2017-03-02 11:31:41 +1100

    WIP

---
+++ webapp/block/DealerCommentsBlock.js
@@ -0,0 +1,11 @@
+sap.ui.define(["sap/uxap/BlockBase"
+], function ( BlockBase) {
+	"use strict";
+ 
+	return BlockBase.extend("WarrantyClaim_MockUp.block.DealerCommentsBlock", {
+		metadata: {
+			/* no additional views provided */
+		}
+	});
+
+}, true);
---
+++ webapp/block/HondaAssessmentBlock.js
@@ -0,0 +1,11 @@
+sap.ui.define(["sap/uxap/BlockBase"
+], function ( BlockBase) {
+	"use strict";
+ 
+	return BlockBase.extend("WarrantyClaim_MockUp.block.HondaAssessmentBlock", {
+		metadata: {
+			/* no additional views provided */
+		}
+	});
+
+}, true);
